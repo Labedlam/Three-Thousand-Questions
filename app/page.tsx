14b2be567688f62cf120b9841b272b88e6512bfb
Zeeshan Dawood <dawood.zeeshan@gmail.com>
2023-11-17 11:12:56 -0600

    fix build issue , and env url

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client"
-import Image from 'next/image'
+
 import { useEffect, useState } from 'react'
-import { isNull } from 'util'
+
 //create a debounce hook
 function useDebounce(value: any, delay: number) {
   const [debouncedValue, setDebouncedValue] = useState(value)
@@ -24,7 +24,7 @@ function useDebounce(value: any, delay: number) {
 export default function Home() {
   const [question, setQuestion] = useState('')
   const [questionLength, setQuestionLength] = useState(0)
-  const [numberSelection, setNumberSelection] = useState<number|null>(0)
+  const [numberSelection, setNumberSelection] = useState<number|undefined>()
   const debouncedNumberSelection = useDebounce(numberSelection, 100)
 
   // get local api url
@@ -58,7 +58,7 @@ export default function Home() {
     //if not a number or zero, set to 1
     else {
       setQuestion('')
-      setNumberSelection(null)
+      setNumberSelection(undefined)
     }
   }
 
@@ -74,33 +74,11 @@ export default function Home() {
         })
     }
 
-    //if not valid, fetch default question
-    // else {
-    //   fetch(`${url}/api/questions?num=1`)
-    //     .then((response) => response.json())
-    //     .then((data) => {
-    //       setQuestion(data.results)
-    //     })
-    // }
   }
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {/* number input that allows limits between 1 and 3000 with label "Pick a number" , adds invalid tailwind class when value is null or 0*/}
 
-      {/* <div className="flex flex-col items-center justify-center">
-        <label className="text-xl font-bold">Enter a number up till 3000</label>
-        <input
-        // add tailwind border red when invalid
-          className={numberSelection === null ? "border-2 border-red-500 rounded-lg p-2 m-2" : "border-2 border-black rounded-lg p-2 m-2"}
-          type="number"
-          min="1"
-          max="3000"
-          value={numberSelection}
-          onChange={(e) => handleNumberInput(e)}
-          // className="border-2 border-black rounded-lg p-2 m-2"
-        />
-      </div> */}
       
       {/* tailwind form that takes max width when small, and centered as it grows */}
       <div className="flex flex-col items-center justify-center">
